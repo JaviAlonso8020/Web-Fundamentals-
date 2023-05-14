3726a22cafa4e899b33652b373fe01da0d06aa60
Javier Ramos <122590607+JaviAlonso8020@users.noreply.github.com>
2023-05-14 17:43:34 -0500

    JS Video preview practice assignment

---
+++ video-preview/script.js
@@ -0,0 +1,9 @@
+function playVideo(element){
+    console.log("this should play...");
+    element.play();
+}
+
+function pauseVideo(element){
+    console.log("this should pause")
+    element.pause();
+}
